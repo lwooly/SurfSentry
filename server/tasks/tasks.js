44e679f6d368d2ec7cd85d8397b92a46fc47ca33
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-05-02 19:11:27 +0100

    8am cron

--- server/tasks/tasks.js
+++ server/tasks/tasks.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron'
 import { surfCheck } from './notify/surfCheck.js';
 
-cron.schedule("*/50 * * * *", () => {
+cron.schedule("0 8 * * *", () => {
     console.log("A cron job that runs every 1 minutes");
     surfCheck()
   });
