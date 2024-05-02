63d175eca279b88678f70e8bca0e2c481f95f41c
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-05-02 19:13:29 +0100

    remove fn call

--- server/tasks/notify/surfCheck.js
+++ server/tasks/notify/surfCheck.js
@@ -66,4 +66,4 @@ export const surfCheck = async () => {
   
 };
 
-surfCheck()
+// surfCheck()
--- server/tasks/tasks.js
+++ server/tasks/tasks.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import { surfCheck } from './notify/surfCheck.js';
 
 cron.schedule("0 8 * * *", () => {
-    console.log("A cron job that runs every 1 minutes");
+    console.log("A cron job that runs at 8 am");
     surfCheck()
   });
 
