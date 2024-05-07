0633de2a47f9f2594badd0d1e905917ebfe73447
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-05-07 18:32:23 +0100

    cron time updated to check deployment

--- server/tasks/tasks.js
+++ server/tasks/tasks.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron'
 import { surfCheck } from './notify/surfCheck.js';
 
-cron.schedule("0 8 * * *", () => {
+cron.schedule("* */2 * * *", () => {
     console.log("A cron job that runs at 8 am");
     surfCheck()
   });
