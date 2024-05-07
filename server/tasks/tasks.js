044e89c5102537f962f295b194482ceedec694f3
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-05-07 19:58:44 +0100

    d

--- server/tasks/tasks.js
+++ server/tasks/tasks.js
@@ -1,7 +1,7 @@
 // import cron from 'node-cron'
 // import { surfCheck } from './notify/surfCheck.js';
 
-import { surfCheck } from "./notify/surfCheck"
+import { surfCheck } from "./notify/surfCheck.js"
 
 // cron.schedule("* */2 * * *", () => {
 //     console.log("A cron job that runs at 8 am");
